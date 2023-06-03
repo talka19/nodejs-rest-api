53f3fd61e5127927f22e039aa4e5463b26d9e7d7
talka19 <talka19.10@gmail.com>
2023-06-03 13:57:31 +0300

    refactored to use reexport

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,7 +4,7 @@ const ctrl = require('../../controllers/contacts');
 
 const {validateBody, isValidId} = require('../../middlewares');
 
-const {schemas} = require('../../models/contact');
+const {schemas} = require('../../models');
 
 const router = express.Router()
 
